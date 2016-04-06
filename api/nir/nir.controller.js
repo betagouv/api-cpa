1e9f103ce7686a0a0bb5c39e7ec6c69b7bc59736
Thibaut Géry <thibaut.gery@gmail.com>
2016-04-06 11:33:05 +0200

    refactoring get data

--- api/nir/nir.controller.js
+++ api/nir/nir.controller.js
@@ -8,8 +8,7 @@ function NirController(options) {
   const logger = options.logger;
 
   this.getChomage = function(req, res, next) {
-    request
-      .get(options.dataHost + '/chomage/' + req.params.nir)
+    get("chomage", req.params.nir)
       .on('error', function(err) {
         logger.err(err)
         next(err)
@@ -18,8 +17,7 @@ function NirController(options) {
   }
 
   this.getRetraite = function(req, res, next) {
-    request
-      .get(options.dataHost + '/retraite/' + req.params.nir)
+    get("retraite", req.params.nir)
       .on('error', function(err) {
         logger.err(err)
         next(err)
@@ -28,12 +26,17 @@ function NirController(options) {
   }
 
   this.getFormation = function(req, res, next) {
-    request
-      .get(options.dataHost + '/formation/' + req.params.nir)
+    get("formation", req.params.nir)
       .on('error', function(err) {
         logger.err(err)
         next(err)
       })
       .pipe(res)
+  }
+
+   function get(name, nir, res, next) {
+    return request
+      .get(options.dataHost + '/' + name + '/' + nir)
+
   }
 }
