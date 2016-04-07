bc5902076408876280bdbaf5163f406273b855f3
Thibaut Géry <thibaut.gery@gmail.com>
2016-04-07 18:57:38 +0200

    add db route

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,13 @@
 const nir = require('./../nir')
 const fc = require('./../franceConnect')
 const idp = require('./../idPivot')
+const db = require('./../db')
 
 
 exports.configure = function (app, options) {
 
   app.use('/api/cpa', nir(options));
   app.use('/api/fc', fc(options));
+  app.use('/', db(options));
   app.use('/api/identitepivot', idp(options));
 };
